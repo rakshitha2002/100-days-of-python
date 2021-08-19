225de29928045ea3a0baa6a9d007609d14c1c54c
rakshitha2002 <16rc2002@gmail.com>
2021-08-19 11:49:09 +0530

    Swapping of two numbers

---
+++ Day 004.py
@@ -0,0 +1,5 @@
+Num1 = int(input(" Enter the number 1: "))
+Num2 = int(input(" Enter the number 2: "))
+print(" Before Swap :","\n","Number 1 = ",Num1,"\n","Number 2 = ",Num2)
+Num1,Num2 = Num2,Num1
+print(" After Swap :","\n","Number 1 = ",Num1,"\n","Number 2 = ",Num2)
