945675b453b2253ac54a94d921bcbb87e7265e7c
rakshitha2002 <16rc2002@gmail.com>
2021-08-20 11:55:45 +0530

    Area of Triangle

--- Day 005.py
+++ Day 005.py
@@ -1,4 +1,5 @@
 a = float(input("Enter side a length : "))
 b = float(input("Enter side b length : "))
 c = float(input("Enter side c length : ")) 
-print("Area of Triangle : ",(((a + b + c) / 2)*(((a + b + c) / 2) - a)*(((a + b + c) / 2) - b)*(((a + b + c) / 2) - c)) ** 0.5)
+s = (a + b + c) / 2
+print("Area of Triangle : ",(s * (s - a) * (s - b) * (s - c)) ** 0.5)
