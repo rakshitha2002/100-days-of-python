f0d087dc93260fb5d95e25f7746edbbef9511a99
rakshitha2002 <16rc2002@gmail.com>
2021-08-27 13:15:23 +0530

    Vowel and Consonant Check

--- Day_012.py
+++ Day_012.py
@@ -1,5 +1,9 @@
 def isVowel(letter):
     """Your code here"""
+    if letter in ('a','A','e','E','i','I','o','O','u','U'):
+        return True
+    else:
+        return False
     
 # Make use of this main code to check manual test cases....
 if __name__ == "__main__":
