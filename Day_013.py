63d95d06512319a39d2c724ea319411b8c4ef4f0
rakshitha2002 <16rc2002@gmail.com>
2021-08-28 22:32:32 +0530

    Fibonacci Numbers

--- Day_013.py
+++ Day_013.py
@@ -1,6 +1,13 @@
 def fibonacciNumberGenerator(N):
     """Your code here"""
-    
+    r =[0,1]
+    if N == 1:
+        return [r[0]]
+    else:
+        for i in range(2,N):
+            num = r[-1]+r[-2]
+            r.append(num)
+        return r
 # Make use of this main code to check manual test cases....
 if __name__ == "__main__":
     N = int(input("Enter limit : "))
