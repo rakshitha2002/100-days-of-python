7a54f9c7ff319ce29d4b8dbf28dda3447f04e71e
rakshitha2002 <16rc2002@gmail.com>
2021-08-30 18:26:55 +0530

    Output Generation-I

---
+++ Day_015.py
@@ -0,0 +1,9 @@
+for i in range(1,10):
+    if i == 1 :
+        print(i,end="\n")
+    elif i >= 2 and i < 5:
+        print(i,end="")
+    elif i == 5:
+        print("")
+    if i >= 5:
+        print(i,end="")
