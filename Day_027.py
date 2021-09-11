2c45a098d5f3609c62fa6926393e608ee2f9ec6c
rakshitha2002 <16rc2002@gmail.com>
2021-09-11 21:21:24 +0530

    Zero Sum Subarray/Sublist

--- Day_027.py
+++ Day_027.py
@@ -6,9 +6,8 @@ negative = [ int(i) for i in n if int(i) < 0]
 if len(negative)== 0:
     print("No Zero Sublist found :-(")
 else:
- for i in range(1, len(sub)):
-    if i == 1:
-        print("Zero Sublist found :-)")
+  print("Zero Sublist found :-)")
+  for i in range(1, len(sub)):   
     for sublist in set(combinations(sub, i)):
         if sum(sublist) == 0:
             print(sorted((list(sublist))))
