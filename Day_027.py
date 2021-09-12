45d008aeb6613e3b12f4059c4510aa1ecb137e85
rakshitha2002 <16rc2002@gmail.com>
2021-09-12 11:21:13 +0530

    Zero Sum Subarray/Sublist

--- Day_027.py
+++ Day_027.py
@@ -2,14 +2,14 @@ from itertools import combinations
 
 n = input("Enter list elements : ").split()
 sub = [int(i) for i in n]
-negative = [ int(i) for i in n if int(i) < 0]
-positive = [ int(i) for i in n if int(i) > 0]
-if len(negative)== 0 or len(positive)== 0:
+li= []
+for i in range(1, len(sub)):   
+    for sublist in set(combinations(sub, i)):
+        if sum(sublist) == 0:
+            li.append(sorted((list(sublist))))
+if len(li)== 0 :
     print("No Zero Sublist found :-(")
 else:
   print("Zero Sublist found :-)")
-  for i in range(1, len(sub)):   
-    for sublist in set(combinations(sub, i)):
-        if sum(sublist) == 0:
-            print(sorted((list(sublist))))
-        
+  for i in li:
+    print(i)       
