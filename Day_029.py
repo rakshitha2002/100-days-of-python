961722a9242974b9b8d82b9f735b56251721888d
rakshitha2002 <16rc2002@gmail.com>
2021-09-27 12:43:28 +0530

    String Processing-I

--- Day_029.py
+++ Day_029.py
@@ -11,10 +11,13 @@ if is_consecutive_flag:
     print("All the letters occurs exactly once")
 else:
     print(f"The string {str(k)[1:-1]} which are not consecutive letters")
+    is_value_flag = True
     for i in sorted(set(k)):
-        if k.count(i)>1:
-            print(f"letter '{i}' occurs {k.count(i)} time(s)")
-        else:
-            print("All the letters occurs exactly once")
+        if k.count(i)!=1:
+            is_value_flag=False
             break
-        
+    if is_value_flag:
+          print("All the letters occurs exactly once")
+    else:
+        for i in sorted(set(k)): 
+           print(f"letter {i} occur {k.count(i)}time(s)")       
