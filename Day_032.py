1989c2c30d2c6ace5c8babc4a040c076462c4f0c
rakshitha2002 <16rc2002@gmail.com>
2021-10-01 17:43:17 +0530

    String Processing-IV

--- Day_032.py
+++ Day_032.py
@@ -1,6 +1,6 @@
-#get the input
+# get the input
 f = input("Enter string : ")
-#check alphanumeric and if space also joined
+# check alphanumeric and if space also joined
 h = "".join(i for i in f if i.isalnum() or i == " ")
-#split the string and join the spaces and print the output
+# split the string and join the spaces and print the output
 print("Final result : "+" ".join(h.split()))
